4044073484f89888f7f810eebdbbf33f1f0c127a
Wandole <mchirez@gmail.com>
2022-12-18 22:10:31 +0100

    Remove redux

--- src/feature/form/form.actions.ts
+++
@@ -1,7 +0,0 @@
-import { createAction } from '@reduxjs/toolkit'
-
-const setFirstName = createAction('form/setName', (newValue: string) => ({
-  payload: { newValue },
-}))
-
-export { setFirstName }
