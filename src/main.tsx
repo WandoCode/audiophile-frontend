135caaa7d22a9903b41ac4dc24bfec348a713caa
Wandole <mchirez@gmail.com>
2023-01-18 18:45:54 +0100

    Add todos

--- src/main.tsx
+++ src/main.tsx
@@ -11,3 +11,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     </ContextProvider>
   </React.StrictMode>
 )
+// TODO: version react: vider le cart quand on revient on clique pour revenir sur la page principale
+// TODO:react: Changer btn en link quand nécessaire.
+// TODO: react: adapter la tailler des titre h2--large en ajoutant un breakpoint-down(small = 500px) pour dminuer la taille de la font
+// TODO: put exported types in a types/index.d.ts file
